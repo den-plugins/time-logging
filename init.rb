74f25d6466068d98abc700cc691c8a03daa71d95
Gillian Dugay <gdugay@exist.com>
2012-03-12 11:34:33 +0800

    Added plugin base

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :time_logging do
+  name 'Weekly time logging plugin'
+  author 'Exist DEN Team'
+  description 'Patch for new weekly time logging features'
+  version '0.0.1'
+end
