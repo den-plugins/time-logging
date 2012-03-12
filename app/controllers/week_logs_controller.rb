456d7994d2accd20e999a5d0a15cb78a9ff39681
Geraldine Milan <blackgemph@yahoo.com>
2012-03-12 16:52:06 +0800

    WIP Weekly Time Logging > Display assigned issues

--- app/controllers/week_logs_controller.rb
+++ app/controllers/week_logs_controller.rb
@@ -1,6 +1,10 @@
 class WeekLogsController < ApplicationController
 
   def index
+    @user = User.current
+    issue_conditions = []
+    issue_conditions << "assigned_to_id = #{@user.id}"
+    @issues = Issue.find(:all, :conditions => issue_conditions)
   end
   
   def create
