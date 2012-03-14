49effbf62aba86aef579ffa23506d160ed227695
normz <jndc6@yahoo.com>
2012-03-14 15:00:08 +0800

    additional updates

--- app/models/time_entry_extn.rb
+++ app/models/time_entry_extn.rb
@@ -5,8 +5,12 @@ module TimeEntryExtn
   
   module ClassMethods
     def get_hours_by_date(current_date, issue)
-      ret = TimeEntry.find(:all, :conditions=>["spent_on = ? AND issue_id = ?", current_date, issue.id])
-      ret.empty? ? 0.0 : ret.first.hours
+      arr = []
+      (0..6).each do |val| 
+        ret = TimeEntry.find(:all, :conditions=>["spent_on = ? AND issue_id = ?", current_date+val, issue.id])
+        arr << (ret.empty? ? 0.0 : ret.first.hours)
+      end
+      arr
     end
   end
 end
