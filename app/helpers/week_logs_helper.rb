91fb523984e9e8ec67255380a0164b3ad085a088
Gillian Dugay <gdugay@exist.com>
2012-03-16 13:57:07 +0800

    reverted changes to week logs helper

--- app/helpers/week_logs_helper.rb
+++ app/helpers/week_logs_helper.rb
@@ -1,8 +1,7 @@
 module WeekLogsHelper
   def week_dates(date=Date.today)
     date = @week_start || date
-    # Monday to Saturday + Sunday of the previous week
-    [*date.beginning_of_week..(date.end_of_week - 1.day)].push(date.beginning_of_week - 1)
+    date.beginning_of_week..date.end_of_week
   end
 
   def week_days
