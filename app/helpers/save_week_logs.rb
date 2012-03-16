2e59bb44b763d4d46b2a92ca5827761c359637db
Geraldine Milan <blackgemph@yahoo.com>
2012-03-16 11:38:45 +0800

    Don't save if time entry exceeds more than 24 hours per day

--- app/helpers/save_week_logs.rb
+++ app/helpers/save_week_logs.rb
@@ -5,8 +5,10 @@ module SaveWeekLogs
       hash[issue].each_key do |date|
         time_entry = TimeEntry.find(:all, :conditions => ["user_id=? AND issue_id=? AND spent_on=?", user.id, issue, Date.parse(date)])
         hours = Float hash[issue][date]['hours'] rescue 0.0
+        total_time_entry = TimeEntry.sum(:hours, :conditions => ["user_id=? AND spent_on=?", user.id, Date.parse(date)])
+        total_time_entry += hours if hours
         if time_entry.empty?
-          if(hours > 0)
+          if(hours > 0 && total_time_entry <= 24)
             proj_i = Issue.find(issue)
             new_time = TimeEntry.new(:project => proj_i.project, :issue => proj_i, :user => User.current)
             new_time.hours = Float hours 
@@ -15,7 +17,7 @@ module SaveWeekLogs
             new_time.save!
           end
         else
-          if(hours > 0)
+          if(hours > 0 && total_time_entry <= 24)
             time_entry.first.hours = Float hours
             time_entry.first.save!
           else
