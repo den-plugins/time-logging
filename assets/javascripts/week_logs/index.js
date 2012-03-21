bff658fb4b4ee919387804d271782fc7efe018a1
Geraldine Milan <blackgemph@yahoo.com>
2012-03-21 11:18:28 +0800

    Edit success message

--- assets/javascripts/week_logs/index.js
+++ assets/javascripts/week_logs/index.js
@@ -272,7 +272,7 @@ function initializers() {
       Week.refreshTabIndices();
       $('#ajax-indicator').hide();
       if(taskId && taskId.length > 0) {
-        $('#success_message').text('Successfully added task #' + taskId + '.').removeClass('hidden');
+        $('#success_message').text('Successfully added #' + taskId + '.').removeClass('hidden');
         taskRow = $('#issue-' + taskId);
         if(taskRow.length > 0) {
           $('html, body').animate({scrollTop: taskRow.offset().top}, 1000, function() {
@@ -375,7 +375,7 @@ function initializers() {
       Week.refreshTotalHours();
       Week.refreshTabIndices();
       $.post('/week_logs/remove_task.js', {id: taskId}).success(function() {
-        $('#success_message').text('Successfully removed task #' + taskId + '.').removeClass('hidden');
+        $('#success_message').text('Successfully removed #' + taskId + '.').removeClass('hidden');
       });
 
       if (bValid) {
