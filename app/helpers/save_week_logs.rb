4ebe5dfa7816de4ecdf3f936b419537f830b2e17
Normz <johnnormancapule@gmail.com>
2012-03-28 16:48:45 +0800

    Fixed #54317

--- app/helpers/save_week_logs.rb
+++ app/helpers/save_week_logs.rb
@@ -42,21 +42,13 @@ module SaveWeekLogs
         hours = hash[issue][date].to_hours
         total_time_entry = TimeEntry.sum(:hours, :conditions => ["user_id=? AND spent_on=?", user.id, Date.parse(date)])
         total_time_entry += hours
-        if time_entry.empty?
-          if(hours > 0 && flag)
-            new_time = TimeEntry.new(:project => proj_issue.project, :issue => proj_issue, :user => User.current)
-            new_time.hours = hours
-            new_time.spent_on = Date.parse(date)
-            new_time.activity_id = 9
-            new_time.save!
-          end
-        else
-          if(hours > 0 && flag)
-            time_entry.first.hours = hours
-            time_entry.first.save!
-          elsif(hours<=0)
-            time_entry.first.destroy
-          end
+        if(hours > 0 && flag)
+          time_entry.each {|te| te.destroy} if !time_entry.empty?
+          new_time = TimeEntry.new(:project => proj_issue.project, :issue => proj_issue, :user => User.current)
+          new_time.hours = hours
+          new_time.spent_on = Date.parse(date)
+          new_time.activity_id = 9
+          new_time.save!
         end
       end
 
