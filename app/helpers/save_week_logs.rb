7ad2889cb0c670bfe8670a51e7431dcb191bc561
Normz <johnnormancapule@gmail.com>
2012-04-13 12:51:01 +0800

    #55034

--- app/helpers/save_week_logs.rb
+++ app/helpers/save_week_logs.rb
@@ -31,13 +31,12 @@ module SaveWeekLogs
           if project.project_type.scan(/^(Admin)/).flatten.present?
             flag = true
           else
-            if((issue_is_billable && member.first) || (!issue_is_billable && member.first))
-              if(member.first.allocated?(Date.parse(date)))
-                flag = true
-              else
-                error_messages[issue] << "User is not allocated/billable in #{project.name} on #{Date.parse(date).strftime("%m/%d/%Y")}."
-                flag = false
-              end
+            if(((issue_is_billable && member.first && !member.first.shadow?(Date.parse(date))) || (!issue_is_billable && member.first)) && 
+               member.first.allocated?(Date.parse(date)))
+              flag = true
+            else
+              error_messages[issue] << "User is not allocated/billable in #{project.name} on #{Date.parse(date).strftime("%m/%d/%Y")}."
+              flag = false
             end
           end
         end
