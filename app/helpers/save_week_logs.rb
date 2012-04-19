88c90430b9db3983cb9115da244ab4b72ff3f016
Normz <johnnormancapule@gmail.com>
2012-04-19 17:26:54 +0800

    refactoring controller

--- app/helpers/save_week_logs.rb
+++ app/helpers/save_week_logs.rb
@@ -126,4 +126,74 @@ module SaveWeekLogs
     end
     [hash, error_messages]
   end
+
+  def self.add_task(proj_cache, non_proj_cache, issues, params)
+    error_messages = []
+    user = User.current
+    issue_type = params[:type].to_s
+    date = Date.parse(params[:week_start])
+    params[:id].each do |id|
+      alloc_flag = false
+      b_alloc_flag = false
+      issue_id = id.to_i
+      issue = issues[issue_type].find {|param| param.id == issue_id}
+      if issue_type == 'admin'
+        if issue = Issue.find(issue_id)
+          project = issue.project
+          issue = nil if !(issue_type == 'admin' && project.name.downcase['admin'] && project.project_type.downcase['admin'])
+        end
+      end
+      if issue
+        project = issue.project
+        admin_flag = project.project_type.scan(/^(Admin)/).flatten.present?
+        if project.accounting
+          project.accounting.name=="Billable" ? issue_is_billable = true : issue_is_billable = false
+        else
+          issue_is_billable = false
+        end
+        member = project.members.select {|member| member.user_id == user.id}.first
+        
+        if member
+          (date..date.end_of_week).each do |d|
+            alloc_flag=true if member.allocated? d
+            b_alloc_flag=true if member.b_alloc? d
+          end
+        end
+        
+        if !issue_is_billable && member && !alloc_flag && !admin_flag 
+          error_messages << "You are not allocated in issue ##{issue.id} this week."
+        elsif issue_is_billable && member && !b_alloc_flag && !admin_flag
+          error_messages << "You are not billable in issue ##{issue.id} this week."
+        elsif !member
+          error_messages << "You are not a member of #{issue.project.name}." 
+        else
+          case issue_type
+            when 'project'
+              proj_cache << issue_id
+            when 'admin'
+              non_proj_cache << issue_id
+          end
+        end
+      else
+        other_issues = case issue_type
+                       when 'admin'
+                         issues['project']
+                       when 'project'
+                         issues['admin']
+                       end
+        if other_issues.map(&:id).include? issue_id
+          phrase = (issue_type == 'admin' ? 'an admin' : 'a project')
+          error_messages << "Issue ##{issue_id} is not #{phrase} task."
+        elsif Issue.exists? issue_id
+          error_messages << "You are not allowed to log time in issue ##{issue_id}."
+        else
+          error_messages << "Issue ##{issue_id} does not exist."
+        end
+      end
+    end
+    puts error_messages.inspect
+    puts proj_cache
+    puts non_proj_cache
+    [error_messages, proj_cache, non_proj_cache]
+  end
 end
