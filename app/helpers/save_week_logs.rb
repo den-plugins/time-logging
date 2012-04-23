fa2b5d1970d1b36eeae80cd4e711e20317773879
Geraldine Milan <blackgemph@yahoo.com>
2012-04-23 14:53:31 +0800

    Bug #55129. My Time Logs - Default Comment

--- app/helpers/save_week_logs.rb
+++ app/helpers/save_week_logs.rb
@@ -47,6 +47,7 @@ module SaveWeekLogs
           new_time.hours = hours
           new_time.spent_on = Date.parse(date)
           new_time.activity_id = 9
+          new_time.comments = "Logged spent time. Doing #{new_time.activity.name} on #{proj_issue.subject}."
           unless new_time.save
             error_messages[issue] << new_time.errors.full_messages
           end
