b3caf3c9c8d103ad6c4210b084538405237443dd
Normz <johnnormancapule@gmail.com>
2012-06-05 17:24:32 +0800

    updated init

--- init.rb
+++ init.rb
@@ -1,7 +1,6 @@
 require 'redmine'
 require File.dirname(__FILE__) + '/app/helpers/save_week_logs'
 require File.dirname(__FILE__) + '/app/models/time_entry_extn'
-require File.dirname(__FILE__) + '/app/helpers/extend_account_controller'
 config.cache_store = :memory_store
 $redis = Redis.new
 
