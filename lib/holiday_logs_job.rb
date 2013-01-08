039ea7e2f7bbbc0275ac374305c692aee79d9daa
Geraldine Milan <blackgemph@yahoo.com>
2013-01-08 13:10:11 +0800

    modify holiday log job

--- lib/holiday_logs_job.rb
+++ lib/holiday_logs_job.rb
@@ -4,68 +4,55 @@ class HolidayLogsJob
   run_every(Time.parse("12am") + 1.minute)
 
   def perform
-    holiday_job_log ||= Logger.new("#{Rails.root}/log/holiday_job.log")
-    holiday = Holiday.find(:all, :conditions => ["event_date = ?", Date.today])[0]
-    if holiday && holiday.event_date.wday != 6 && holiday.event_date.wday != 0
+    @holiday_job_log ||= Logger.new("#{Rails.root}/log/holiday_job.log")
+    @holiday = Holiday.find(:all, :conditions => ["event_date = ?", Date.today])[0]
+    if @holiday && @holiday.event_date.wday != 6 && @holiday.event_date.wday != 0
       users = User.all(:conditions => "status = #{User::STATUS_ACTIVE} and is_engineering = #{true} or skill = 'Sys Ad'")
       get_location
 
       users.each do |user|
-        current_day_hours = TimeEntry.find(:all, :conditions => ["user_id=? and spent_on=?", user.id, holiday.event_date]).sum(&:hours).to_f
+        current_day_hours = TimeEntry.find(:all, :conditions => ["user_id=? and spent_on=?", user.id, @holiday.event_date]).sum(&:hours).to_f
         if current_day_hours == 0.0
+          @number_of_hours = 8.0
+          @total_allocation = 0.0
+          holiday_location = Holiday::LOCATIONS[@holiday.location]
 
-          total_allocation = 0
-          holiday_location = Holiday::LOCATIONS[holiday.location]
-
-          members = user.members
+          members = user.members.reject { |v| v.project.project_type != "Development" }
 
           members.each do |member|
 
             allocations = member.resource_allocations
 
             allocations.each do |allocation|
-              if allocation.start_date <= holiday.event_date && allocation.end_date >= holiday.event_date &&
+              if allocation.start_date <= @holiday.event_date && allocation.end_date >= @holiday.event_date &&
                   allocation.resource_allocation > 0 &&
                   holiday_location.downcase.include?(@locations[allocation.location].downcase)
-                total_allocation = get_total_allocation(members, holiday)
+                @total_allocation = get_total_allocation(members, "Billable")
 
                 if member.project.accounting_type == "Billable"
 
-                  if total_allocation == 100 || total_allocation < 100
+                  if @total_allocation == 100 || @total_allocation < 100
                     if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Billable"] || allocation.resource_type == Hash[ResourceAllocation::TYPES]["Non-billable"]
-                      timelog(holiday, holiday_job_log, user, member, allocation, "project")
-                    else
-                      timelog(holiday, holiday_job_log, user, member, allocation, "admin")
+                      timelog(user, member, allocation)
                     end
 
-                  elsif total_allocation > 100
-                    total_billable_allocation = get_total_allocation(members, holiday, "Both")
-                    total_shadow_allocation = get_total_allocation(members, holiday, "Project Shadow")
+                  elsif @total_allocation > 100
+                    total_billable_allocation = get_total_allocation(members, "Both")
+                    total_shadow_allocation = get_total_allocation(members, "Project Shadow")
                     if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Billable"] || allocation.resource_type == Hash[ResourceAllocation::TYPES]["Non-billable"]
                       if total_billable_allocation >= 100 && total_shadow_allocation > 0
-                        timelog(holiday, holiday_job_log, user, member, allocation, "project")
+                        timelog(user, member, allocation)
                       else
-                        timelog_over_allocation(total_allocation, holiday, holiday_job_log, user, member, allocation, "project")
+                        timelog_over_allocation(user, member, allocation)
                       end
-                    else
-                      timelog_over_allocation(total_allocation, holiday, holiday_job_log, user, member, allocation, "admin") unless total_billable_allocation >= 100 && total_shadow_allocation > 0
                     end
-                  end
-                else
-                  if total_allocation == 100 || total_allocation < 100
-                    timelog(holiday, holiday_job_log, user, member, allocation, "admin")
-                  elsif total_allocation > 100
-                    tmp_total_allocation = get_total_allocation(members, holiday, "Billable")
-                    timelog_over_allocation(total_allocation, holiday, holiday_job_log, user, member, allocation, "admin") unless tmp_total_allocation == 100
                   end
                 end
               end
             end
           end
-
-          if total_allocation < 100
-            engineer_admin_under_allocation(total_allocation, holiday, holiday_job_log, user)
-          end
+          @total_spent_time = TimeEntry.find(:all, :conditions => ["user_id=? and spent_on=?", user.id, @holiday.event_date]).sum(&:hours).to_f
+          engineer_admin_under_allocation(user) if @total_spent_time < @number_of_hours
         end
       end
     end
@@ -74,26 +61,25 @@ class HolidayLogsJob
 
   private
 
-  def timelog_over_allocation(total_allocation, holiday, holiday_job_log, user, member, allocation, type)
-    project = get_project(type, member)
+  def timelog_over_allocation(user, member, allocation)
+    project = get_project(member)
     issue = get_holiday_issue(project, user)
-    hours_spent = "%.2f" % (8.00 * allocation.resource_allocation/total_allocation).to_f
-    save_time_entry(holiday, issue, project, user, hours_spent, holiday_job_log)
+    hours_spent = "%.2f" % (@number_of_hours * allocation.resource_allocation/@total_allocation).to_f
+    save_time_entry(issue, project, user, hours_spent)
   end
 
-  def timelog(holiday, holiday_job_log, user, member, allocation, type)
-    project = get_project(type, member)
+  def timelog(user, member, allocation)
+    project = get_project(member)
     issue = get_holiday_issue(project, user)
-    hours_spent = "%.2f" % (8.00 * allocation.resource_allocation/100).to_f
-    save_time_entry(holiday, issue, project, user, hours_spent, holiday_job_log)
+    hours_spent = "%.2f" % (@number_of_hours * allocation.resource_allocation/100).to_f
+    save_time_entry(issue, project, user, hours_spent)
   end
 
-  def engineer_admin_under_allocation(total_allocation, holiday, holiday_job_log, user)
-    project = get_project("admin")
+  def engineer_admin_under_allocation(user)
+    project = get_project()
     issue = get_holiday_issue(project, user)
-    diff_alloc = 100 - total_allocation
-    hours_spent = "%.2f" % (8.00 * diff_alloc/100).to_f
-    save_time_entry(holiday, issue, project, user, hours_spent, holiday_job_log)
+    hours_spent = "%.2f" % (@number_of_hours - @total_spent_time).to_f
+    save_time_entry(issue, project, user, hours_spent)
   end
 
   def get_location
@@ -104,8 +90,8 @@ class HolidayLogsJob
     end
   end
 
-  def get_project(type, member=nil)
-    unless type == "project"
+  def get_project(member=nil)
+    unless member && member.project.accounting_type == "Billable"
       Project.find_by_identifier("existengradmn")
     else
       project_parent = member.project.parent
@@ -130,51 +116,55 @@ class HolidayLogsJob
   end
 
 
-  def save_time_entry(holiday, issue, project, user, hours_spent, holiday_job_log)
+  def save_time_entry(issue, project, user, hours_spent)
     if issue && project && user
       new_time = TimeEntry.new(:project => project, :issue => issue, :user => user,
-                               :spent_on => holiday.event_date, :activity_id => 9, :hours => hours_spent)
+                               :spent_on => @holiday.event_date, :activity_id => 9, :hours => hours_spent)
       new_time.comments = "Logged spent time. Doing #{new_time.activity.name} on #{new_time.issue.subject}"
       if new_time.save
-        holiday_job_log.info("Added #{hours_spent} hours on #{holiday.event_date.to_s} to #{issue.subject} of project #{project} for #{user.login}")
+        @holiday_job_log.info("Added #{hours_spent} hours on #{@holiday.event_date.to_s} to #{issue.subject} of project #{project} for #{user.login}")
       else
-        holiday_job_log.info("Failed to add #{hours_spent} hours on #{holiday.event_date.to_s} to #{issue.subject} of project #{project} for #{user.login} : #{new_time.errors.full_messages}")
+        @holiday_job_log.info("Failed to add #{hours_spent} hours on #{@holiday.event_date.to_s} to #{issue.subject} of project #{project} for #{user.login} : #{new_time.errors.full_messages}")
       end
     end
   end
 
-  def get_total_allocation(members, holiday, acctg=nil)
+  def get_total_allocation(members, acctg=nil)
     sum = 0
     members.each do |member|
-      sum += get_alloc(member, holiday, acctg)
+      sum += get_alloc(member, acctg)
     end
     sum
   end
 
-  def get_alloc(member, holiday, acctg=nil)
+  def get_alloc(member, acctg=nil)
     allocations = member.resource_allocations
-    total_allocation = 0
-    holiday_location = Holiday::LOCATIONS[holiday.location]
+    allocation_total = 0
+    holiday_location = Holiday::LOCATIONS[@holiday.location]
     get_location
     unless allocations.empty?
       allocations.each do |allocation|
-        if allocation.start_date <= holiday.event_date && allocation.end_date >= holiday.event_date &&
+        if allocation.start_date <= @holiday.event_date && allocation.end_date >= @holiday.event_date &&
             allocation.resource_allocation > 0
           case acctg
             when "Billable"
-              total_allocation += allocation.resource_allocation if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Billable"]
+              allocation_total += allocation.resource_allocation if allocation.resource_type == (Hash[ResourceAllocation::TYPES]["Billable"] ||
+                  allocation.resource_type == Hash[ResourceAllocation::TYPES]["Non-billable"] ||
+                  allocation.resource_type == Hash[ResourceAllocation::TYPES]["Project Shadow"]) &&
+                  member.project.acctg_type == Enumeration.find_by_name("Billable").id
             when "Both"
-              total_allocation += allocation.resource_allocation if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Billable"] ||
-                  allocation.resource_type == Hash[ResourceAllocation::TYPES]["Non-billable"]
+              allocation_total += allocation.resource_allocation if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Billable"] ||
+                  allocation.resource_type == Hash[ResourceAllocation::TYPES]["Non-billable"] &&
+                      member.project.acctg_type == Enumeration.find_by_name("Billable").id
             when "Project Shadow"
-              total_allocation += allocation.resource_allocation if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Project Shadow"]
+              allocation_total += allocation.resource_allocation if allocation.resource_type == Hash[ResourceAllocation::TYPES]["Project Shadow"]
             else
-              total_allocation += allocation.resource_allocation
+              allocation_total += allocation.resource_allocation
           end
         end
       end
     end
-    total_allocation
+    allocation_total
   end
 
 end
